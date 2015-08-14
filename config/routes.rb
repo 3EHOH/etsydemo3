96bc24c46b913eb25552a988d558f8f1b2ad69da
NoahJohnRondeau <zenonevans@gmail.com>
2015-08-14 15:13:06 -0700

    temp admin change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :admins, :skip => :registrations
+  devise_for :admins
   devise_for :users
   resources :listings do 
     resources :orders, only: [:new, :create]
